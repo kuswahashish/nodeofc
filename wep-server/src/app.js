0fd1efbadb7eebf1778d09ea159bc5458177ceaf
kuswahashish <kuswahashish@gmail.com>
2021-11-17 22:47:22 +0530

    created web server using express and created routes for templates and used partials

--- wep-server/src/app.js
+++ wep-server/src/app.js
@@ -1,15 +1,59 @@
 const path= require('path');
-const express = require('express')
+const hbs = require('hbs')
+const express = require('express');
 
 const app = express()
-
+//Defined Path
 const publicPath = path.join(__dirname, '../public')
+const templatePath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+//Setup path for views and tamplates
+app.set('view engine','hbs')
+app.set('views',templatePath)
+hbs.registerPartials(partialsPath)
+
+//static directory 
 app.use(express.static(publicPath))
 
+app.get('',(req, res) => {
+    // console.log(req.params) 
+    res.render('index',{
+        htitle: 'Dynamic pages',
+        title: 'Weather App',
+        name:'ashish'
+    })
+})
+app.get('/about',(req, res) => {
+    res.render('about',{
+        htitle: 'Dynamic pages',
+        title: 'About page',
+        name:'john'
+    })
+})
+app.get('/help',(req, res) => {
+    res.render('index',{
+        htitle: 'Dynamic pages',
+        title: 'Help Page',
+        name:'wick'
+    })
+})
 app.get('/weather',(req,res)=>{
     res.send({ 
         weather : 'its cloudy',
         location : 'ahmedabad'
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('articalerror',{
+        htitle: 'Dynamic pages',
+      
+    })
+})
+app.get('*',(req,res)=>{
+    res.render('404error',{
+        htitle: 'Dynamic pages',
+      
     })
 })
 app.listen(3000,()=>{
